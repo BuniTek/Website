992b754e41ec7212722af5878a30e8144e780c17
Elie Mugenzi <eliemugenzi@gmail.com>
2020-04-26 15:43:38 +0200

    Added 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,11 +3,25 @@ import React from 'react';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
+import '../assets/styles/pages/notfound.scss';
+
+import ServerDown from '../assets/images/server-down.png';
+
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div className="notfound">
+      <div className="notfound__container">
+        <div className="notfound__text">
+          <h1>Oops!</h1>
+          <p>Dont panic, its just a file that was not found on our server. </p>
+        </div>
+        <img src={ServerDown} alt="Route not found" />
+        <div className="notfound__404">
+          <h1>404</h1>
+        </div>
+      </div>
+    </div>
   </Layout>
 );
 
