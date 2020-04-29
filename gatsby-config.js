24ab3629bacf99646ee3536d38149b39eaec3bd4
billyjason <byiringirobill@gmailcom>
2020-04-29 01:10:09 +0100

    changes side title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
-    description: 'Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.',
+    title: 'AfricaI',
+    description: 'Invent Future by bringing to cool, cut-edge technology to Africa Youth',
     author: '@gatsbyjs',
   },
   plugins: [
