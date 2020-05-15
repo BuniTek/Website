b4a04efe984523743d91f7ce92e8e74eee7d5f5f
billyjason <byiringirobill@gmailcom>
2020-05-15 15:26:34 +0100

    MD extractions and post and slug creation

---
+++ src/layouts/PostLayout.js
@@ -0,0 +1,55 @@
+
+import React from 'react';
+import { graphql } from 'gatsby';
+
+import 'materialize-css/dist/css/materialize.min.css';
+import 'semantic-ui-css/semantic.min.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import 'antd/dist/antd.css';
+// import { useStaticQuery, graphql } from 'gatsby';
+
+import SiteHeader from '../components/Header/index';
+import Footer from '../components/Footer/index';
+import './layout.css';
+
+library.add(fab, faCoffee);
+
+const PostLayout = ({ data }) => {
+
+    const post = data.markdownRemark;
+    return (
+        <>
+          <div>
+            <SiteHeader />
+            <main>
+                <div>
+                    <h1> {post.frontmatter.title} </h1>
+                    <div dangerouslySetInnerHTML = {{__html: post.html}} />
+                </div>
+
+            </main>
+            <Footer />
+          </div>
+        </>
+      );
+}
+
+
+
+
+export default PostLayout;
+
+export const query = graphql`
+
+query($slug: String!){
+    markdownRemark(fields: {slug: {eq: $slug}}){
+        html
+        frontmatter{
+            title
+        }
+    }
+}
+
+`
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -5,19 +5,13 @@ import SEO from '../components/seo';
 import BlogItem from '../components/Blog/BlogItem';
 import BlogSearch from '../components/Blog/Search';
 import { setLogoUrl } from '../redux/actions';
+import {graphql} from 'gatsby';
 
 import '../assets/styles/pages/blog.scss';
 import darkLogo from '../assets/images/africai.png';
 
-const lipsum = `
-It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. 
-The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 
-'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. 
-Various versions have evolved over the years, sometimes by accident,
- sometimes on purpose (injected humour and the like).
-`;
-
-const Blog = () => {
+const Blog = ({data}) => {
+  console.log(data);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(setLogoUrl({ logo: darkLogo }));
@@ -30,22 +24,14 @@ const Blog = () => {
           <h2 className="blog__heading">Blog</h2>
           <BlogSearch />
         </div>
-        <BlogItem
-          title="Python Encryption App"
-          description={lipsum}
-        />
-        <BlogItem
-          title="Advantage of Parcel over Webpack"
-          description={lipsum}
-        />
-        <BlogItem
-          title="Future of AfricaI"
-          description={lipsum}
-        />
-        <BlogItem
-          title="Meet JC Billy, our CEO and Founder"
-          description={lipsum}
-        />
+        {data.allMarkdownRemark.nodes.map(node => (
+          <BlogItem
+            title={node.frontmatter.title}
+            description={node.excerpt}
+            readMore = {node.fields.slug}
+          />
+        ))}
+       
       </div>
 
     </Layout>
@@ -54,3 +40,24 @@ const Blog = () => {
 };
 
 export default Blog;
+
+export const query = graphql` {
+  allMarkdownRemark {
+    nodes {
+      frontmatter {
+        title
+        date
+        keywords
+        author
+        image
+      }
+      excerpt
+      html
+      fields {
+        slug
+      }
+    }
+  }
+}
+`
+ 
