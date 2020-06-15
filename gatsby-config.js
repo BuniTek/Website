64261ccb184cdfbfe347b18df9d5c8ab33dbe6f5
billyjason <byiringirobill@gmailcom>
2020-06-15 03:37:27 +0100

    added a datalayer

--- gatsby-config.js
+++ gatsby-config.js
@@ -58,7 +58,12 @@ module.exports = {
         // should be an object or a function that is executed in the browser
         //
         // Defaults to null
-        defaultDataLayer: { platform: "gatsby" },
+        
+        defaultDataLayer: function () {
+          return {
+            pageType: window.pageType,
+          }
+        },
   
         // Specify optional GTM environment details.
         // gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
