a4f2d15c6dc023012f5fa55e4e7075ef8247c30f
willyserge <willyserge01@gmail.com>
2020-07-03 13:03:25 +0200

    add search query state to the root reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
 import uiReducer from './ui.reducer';
-
+import searchReducer from './search.reducer';
 export default combineReducers({
   ui: uiReducer,
+  query: searchReducer
 });
