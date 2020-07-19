bca7f3f80e1d494f6b3bc614634c3b3400a25aeb
willyserge <willyserge01@gmail.com>
2020-07-19 15:40:28 +0200

    add  more sections to about-page

---
+++ src/components/InfoCard/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './index.scss';
+
+function InfoCard() {
+  return (
+    <div className="info__card">
+      <div>
+        <h3>About Courses</h3>
+        <p>
+          this is course content this is course content this is course content
+          this is course content this is course content this is course content
+          this is course content this is course content this is course content
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default InfoCard;
--- src/pages/about.js
+++ src/pages/about.js
@@ -2,21 +2,26 @@ import React, { useEffect } from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
 import { useDispatch } from 'react-redux';
 import Img from 'gatsby-image';
+
 import Team from '../components/Team';
 import Layout from '../layouts/layout';
 import SEO from '../components/seo';
-import Link from '../components/Link';
+import InfoCard from '../components/InfoCard';
+import Line from '../components/VerticalLine';
 import { setLogoUrl, setFooterVisible } from '../redux/actions';
 
 import '../assets/styles/pages/about.scss';
 import logo from '../assets/images/africai_dark.png';
 import quoteImage from '../assets/images/quote-right-solid.svg';
+
+
 import edinburghLogo from '../assets/images/edinburgh.png';
 import msLogo from '../assets/images/ms.png';
 import ibmLogo from '../assets/images/ibm.png';
 import armLogo from '../assets/images/arm.png';
 
-const AboutUs = ({ data: { storyImage, schoolImage, missionImage} }) => {
+
+const AboutUs = ({ data: { storyImage, schoolImage, missionImage } }) => {
   const dispatch = useDispatch();
   useEffect(() => {
     const body = document.querySelector('body');
@@ -28,72 +33,140 @@ const AboutUs = ({ data: { storyImage, schoolImage, missionImage} }) => {
     setFooterVisible({ visible: true })(dispatch);
   }, []);
 
-  
+
   return (
     <Layout>
       <SEO title="About Us" />
       <div className="about">
         <div className="about__container">
-          <div className="about__story">
-          <img src={quoteImage} className="about__story--quote" />
-            <div className="about__flex">
-              <div className="about__flex--image">
-                <Img fluid={storyImage.childImageSharp.fluid} alt="Our story" />
+
+
+          <section>
+            <div className="about__first-section">
+              <div className="first-section__image">
+                <Img fluid={storyImage.childImageSharp.fluid} alt="Our story" style={{ border: '2px solid black' }} />
+              </div>
+              <div className="section__content">
+                <div className="about__shadow rounded absolute-right" />
+                <div className="section__sub-content">
+                  <div className="section__title--container">
+                    <span className="section__title">Who Are WE ?</span>
+                    <img src={quoteImage} alt="quote" />
+                  </div>
+                  <div>
+                    <p>proprietary curriculum curated to be hands-on, fun and Africa specifkjfjfnkjfnjkfnfjnf fofnjfnfifnfofnfoinffif</p>
+                    <p>proprietary curriculum curated to be hands-on, fun and Africa specifkjfjfnkjfnjkfnfjnf fofnjfnfifnfofnfoinffif</p>
+                    <p>proprietary curriculum curated to be hands-on, fun and Africa specifkjfjfnkjfnjkfnfjnf fofnjfnfifnfofnfoinffif</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+
+          <section className="about__second-section-container">
+            <div className="about__second-section">
+              <div className="section__content">
+                <div className="about__shadow circle absolute-left" />
+                <div className="section__sub-content">
+                  <div className="section__title--container">
+
+                    <img src={quoteImage} alt="quote" className="quote__heading-rotated" />
+                    <span className="section__title">Our Mission ?</span>
+                  </div>
+                  <div className="section__content-description " style={{ paddingLeft: 90, paddingTop: 60 }}>
+                    <p>proprietary curriculum curated to be hands-on, fun and Africa specifkjfjfnkjfnjkfnfjnf fofnjfnfifnfofnfoinffif</p>
+                    <p>proprietary curriculum curated to be hands-on, fun and Africa specifkjfjfnkjfnjkfnfjnf fofnjfnfifnfofnfoinffif</p>
+                    <p>proprietary curriculum curated to be hands-on, fun and Africa specifkjfjfnkjfnjkfnfjnf fofnjfnfifnfofnfoinffif</p>
+                  </div>
+                </div>
+              </div>
+              <div>
+                <Img fluid={storyImage.childImageSharp.fluid} alt="Our story" style={{ border: '2px solid black' }} />
+              </div>
+            </div>
+          </section>
+
+
+          <div className="about__third-section-container">
+            <div className="about__third-section">
+              <div className="section__title--container" style={{ textAlign: 'center' }}>
+                <span className="section__title">Why Are We Doing This?</span>
+                <img src={quoteImage} alt="quote" />
               </div>
               <div className="about__flex--description flex-right">
                 <div className="about__shadow rounded absolute-right" />
-                <h1>A Purpose-Driven Company</h1>
-                <p>We belive everyone can learn tech and can use it to create, and impact his/her community</p>
-                <p>We create and deliver hands-On, fun, future-relavant tech courses</p>
+
+                <div>
+                  <ol className="imglist">
+
+                    <li>
+                      <span>
+                        We belive everyone can learn tech and can use it to create, and impact his/her community
+                        We belive everyone can learn tech and can use it to create, and impact his/her community
+                        We belive everyone can learn tech and can use it to create, and impact his/her community
+                      </span>
+                    </li>
+                    <li>
+                      <span>
+                        We belive everyone can learn tech and can use it to create, and impact his/her community
+                        We belive everyone can learn tech and can use it to create, and impact his/her community
+                        We belive everyone can learn tech and can use it to create, and impact his/her community
+                      </span>
+                    </li>
+                  </ol>
+
+                </div>
               </div>
             </div>
           </div>
-          <div className="about__community">
-            <img src={quoteImage} className="about__community--quote" />
-            <div className="about__flex">
-              <div className="about__flex--description flex-left">
-                <div className="about__shadow circle absolute-left" />
-                <h1>Africa-specific Services</h1>
-                <p> 
-                We developed proprietary curriculum curated to be hands-on, fun and Africa speci c; through clubs in various schools across Africa at which A Purpose-Driven Company Create, offer, skills, simple, fun, movement, technology, future relavant Learn more > Africa speci c; through clubs in various schools across Africa at which the community will start; seasonal boot-camps; private tutoring; hackathons and challenges; and Africa-I invent lab which will be creating education-empowering tools to be shared with education institutions.
-                </p>
-                <Link to="/contact">Contact us &gt;</Link>
+
+
+          <section className="about__fourth--section-container">
+
+            <div className="about__fourth--section">
+              <div className="section__title--container" style={{ textAlign: 'center' }}>
+                <img src={quoteImage} className="quote__heading-rotated" alt="quote" />
+                <span className="section__title">About Our Courses</span>
               </div>
-              <div className="about__flex--image">
-                <Img fluid={schoolImage.childImageSharp.fluid} alt="Our Community" />
-              </div>
-            </div>
-          </div>
-          <div className="about__mission">
-          <img src={quoteImage} className="about__story--quote" />
-            <div className="about__flex">
-              <div className="about__flex--image">
-                <Img fluid={missionImage.childImageSharp.fluid} alt="Our Mission" />
-              </div>
-              <div className="about__flex--description flex-right">
-              <div className="about__shadow rounded absolute-right" />
-                <h1>Changing The Ordinary</h1>
-                <p>
-                At AfricaI, We are working to create a movement that will inspire and introduce cutting-edge technologies to young people and demonstrate the potential of those technologies through hands-on, fun and simple scheme. We aim to inspire young people to start solving local problems at a young age and to pursue technology-related  elds. We envisage to create a community of con dent technologists, creative thinkers, and thoughtful learners of our increasingly tech-enabled future. proprietary curriculum curated to be hands-on, fun and Africa speci c; through clubs in various schools across Africa at which the community will start; seasonal boot-camps; private tutoring; hackathons and challenges; and Africa-I invent lab which will be creating educationempowering tools to be shared with education institutions.
-                </p>
+
+              <div className="section__three-grid">
+                <div className="section__grid-item">
+                  <InfoCard />
+                  <InfoCard />
+                </div>
+                <div className="section__grid-line-divider"><Line color="black" /></div>
+                <div className="section__grid-item">
+                  <InfoCard />
+                  <InfoCard />
+                </div>
               </div>
             </div>
-          </div>
-          <div className="about__community">
-            <img src={quoteImage} className="about__community--quote" />
-            <div className="about__flex">
-              <div className="about__flex--description flex-left">
-                <div className="about__shadow circle absolute-left" />
-                <h1>Why Africa-I?</h1>
-                <p>
-                We realised that most young persons do not get a chance to discover or explore their passion in technology and to experiment on its possibilities since a young age; an occurrence that has made technology remain an alien concept to most of our young people in Africa. - We realise that as industry 4.0 approaches, over 25% of jobs will come from the technology sector. Economic advancements will also be fully anchored on this sector. Therefore, if we are to create enough jobs for our growing working population, we need as many people as possible gaining tech mastery through specialising in areas that they love – this can only happen if they are given a fair chance to sample and experience and imagine what’s possible.
-                </p>
+          </section>
+
+          <section className="about__fifth--section-container">
+
+            <div className="about__fifth--section">
+              <div className="section__title--container" style={{ textAlign: 'center' }}>
+
+                <span className="section__title">Content Delivery</span>
+                <img src={quoteImage} alt="quote" />
               </div>
-              <div className="about__flex--image">
-                <Img fluid={schoolImage.childImageSharp.fluid} alt="Our Community" />
+
+              <div className="section__three-grid">
+                <div className="section__grid-item">
+                  <InfoCard />
+                  <InfoCard />
+                </div>
+                <div className="section__grid-line-divider"><Line color="black" /></div>
+                <div className="section__grid-item">
+                  <InfoCard />
+                  <InfoCard />
+                </div>
               </div>
             </div>
-          </div>
+          </section>
+
+
         </div>
         <Team />
         <div className="partners">
