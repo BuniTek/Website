4acaa087f3b4d21cf0bf08ab7eb52d064d7ce25b
alainerosprestige <erosalain23@gmail.com>
2020-07-24 15:38:06 +0200

    home page refactoring.

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,20 +1,19 @@
-import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
-import { Link } from 'gatsby';
-import styled from 'styled-components';
-import { MenuOutlined } from '@ant-design/icons';
-import './index.scss';
-import Sidebar from './Sidebar';
+import React, { useState } from "react"
+import { useSelector } from "react-redux"
+import { Link } from "gatsby"
+import styled from "styled-components"
+import { MenuOutlined } from "@ant-design/icons"
+import "./index.scss"
+import Sidebar from "./Sidebar"
 
 const Logo = styled.img`
-  width: 75px;
-  height: 75px;
-  padding-top: 30px;
-`;
+  width: 5rem;
+  height: auto;
+`
 
 const Header = () => {
-  const [open, setOpen] = useState(false);
-  const logoUrl = useSelector((state) => state.ui.logoUrl);
+  const [open, setOpen] = useState(false)
+  const logoUrl = useSelector(state => state.ui.logoUrl)
 
   return (
     <header>
@@ -40,7 +39,11 @@ const Header = () => {
                 </Link>
               </li>
               <li>
-                <Link to="/courses" activeClassName="nav__active" partiallyActive>
+                <Link
+                  to="/courses"
+                  activeClassName="nav__active"
+                  partiallyActive
+                >
                   Courses
                 </Link>
               </li>
@@ -65,10 +68,11 @@ const Header = () => {
               onClick={() => setOpen(!open)}
               type="button"
             >
-              <MenuOutlined style={{
-                padding: 8,
-                fontSize: 18,
-              }}
+              <MenuOutlined
+                style={{
+                  padding: 8,
+                  fontSize: 18,
+                }}
               />
             </button>
           </div>
@@ -76,7 +80,7 @@ const Header = () => {
       </nav>
       <Sidebar open={open} setOpen={setOpen} />
     </header>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
