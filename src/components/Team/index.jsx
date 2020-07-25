02e6ebac0af373184e8c8bc05e7140a8d11586fe
willyserge <willyserge01@gmail.com>
2020-07-25 18:14:41 +0200

    add join-team button

--- src/components/Team/index.jsx
+++ src/components/Team/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import TeamMember from './TeamMember';
-
+import Button from '../Button';
 import './index.scss';
 
 const Team = () => (
@@ -50,6 +50,9 @@ const Team = () => (
         name="Erastus"
         profileUrl="https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
       />
+    </div>
+    <div className="join__team-button">
+      <Button>Join Team</Button>
     </div>
   </div>
 );
