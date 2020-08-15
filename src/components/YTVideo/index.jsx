652364bfcd75d46871fdbe39eb881bc06a74d847
billyjason <byiringirobill@gmailcom>
2020-08-15 21:58:59 +0100

    added our vimeo video

--- src/components/YTVideo/index.jsx
+++ src/components/YTVideo/index.jsx
@@ -4,7 +4,7 @@ import './index.scss';
 const Video = () => (
   <div className="video-container">
     <iframe
-      src="https://player.vimeo.com/video/444369368?color=ffffff&title=0&byline=0&portrait=0&badge=0"
+      src="https://player.vimeo.com/video/448154640?color=ffffff&title=0&byline=0&portrait=0&badge=0"
       width="640"
       height="360"
       frameBorder="0"
