2098199aaed6020e5e29c4430f2b3bf57465c1dd
billyjason <byiringirobill@gmailcom>
2020-08-16 18:44:42 +0100

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-d66899863188498fe142.js"
+    "url": "webpack-runtime-bda38c3310fcc89042b8.js"
   },
   {
     "url": "framework-bcabf1e7b8d3f9a5b85c.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "e51f8350cc0e49805a4f006cdddf6ce9"
+    "revision": "25fe85d6cc8ade49a94085fed238f452"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-9830ee9fedb160ce2148.js"
